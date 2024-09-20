147b48233c17a1fa5968cf1fd7b90497548b3724
premarumugam <prem.arumugam@commonground.work>
2024-09-20 17:14:29 +0800

    put user data in json file

--- login.js
+++ login.js
@@ -2,15 +2,23 @@ const loginForm = document.getElementById('login-form')
 const loginButton = document.getElementById('login-form-submit')
 const loginErrorMsg = document.getElementById('login-error-msg')
 
-loginButton.addEventListener('click', e => {
-  e.preventDefault()
-  const username = loginForm.username.value
-  const password = loginForm.password.value
-  if (username === 'user' && password === 'webdev') {
-    alert('You have successfully loged in.')
-    location.reload()
-  } else {
-    // change opacity of error message css to be displayed
-    loginErrorMsg.style.opacity = 1
-  }
-})
+async function userLogin() {
+  const usersFetch = await fetch('./users.json') // await can only be used in async functions
+  const users = await usersFetch.json()
+
+  // event listener that waits for a click on the element
+  loginButton.addEventListener('click', e => {
+    e.preventDefault()
+    const username = loginForm.username.value
+    const password = loginForm.password.value
+    if (users.some(u => u.username === username) && users.some(p => p.password === password)) {
+      alert('You have successfully loged in.')
+      location.reload()
+    } else {
+      // change opacity of error message css to be displayed
+      loginErrorMsg.style.opacity = 1
+    }
+  })
+}
+
+userLogin() // when file is called from html, this function executes.
